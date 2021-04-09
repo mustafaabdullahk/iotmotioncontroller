fd7a384b660ae0116d72d099340c4351fa43d5f5
Mustafa Abdullah Kuş <mustafa.kus@sparsetechnology.com>
2021-04-10 02:36:40 +0300

    Add read write and init function

--- ads1256/ads1256.cpp
+++ ads1256/ads1256.cpp
@@ -1,8 +1,56 @@
 #include "ads1256.h"
 
-
+SPI spi(SPI_MOSI, SPI_MISO, SPI_SCK); // mosi, miso, sclk
+DigitalOut cs(SPI_CS);
 
 Analog2Digital::Analog2Digital()
 {
 
+}
+
+int Analog2Digital::Reset()
+{
+    return 0;
+}
+
+int Analog2Digital::WriteCommand(char reg)
+{
+    cs = false;
+    if(spi.write(reg))
+    {
+        cs = true;
+        return 1;
+    }
+    return 0;
+}
+
+int Analog2Digital::WriteRegister(char reg, char data)
+{
+    cs = false;
+    char *txData;
+    txData[0] = CMD_WREG | reg;
+    txData[1] = 0x00;
+    txData[2] = data;
+    char rxData;
+    if(spi.write(txData,sizeof(txData), &rxData, sizeof(rxData)))
+    {
+        cs = true;
+        return 1;
+    }
+    return 0;
+}
+
+int Analog2Digital::ReadData(char reg)
+{
+    cs = false;
+    char *txData;
+    txData[0] = CMD_RREG | reg;
+    txData[1] = 0x00;
+    char rxData;
+    if(spi.write(txData,sizeof(txData), &rxData, sizeof(rxData)))
+    {
+        cs = true;
+        return rxData;
+    }
+    return 0;
 }
